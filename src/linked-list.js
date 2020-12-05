b34ffc642778c3515f01cb05b899201ec6d7e055
Evgeniya Chernaya <evgenya.chernaya@gmail.com>
2020-12-05 14:57:32 -0800

    add SinglyLinkedList class and implement all functionality to the linked list with head, tail and length properties

--- src/linked-list.js
+++ src/linked-list.js
@@ -7,7 +7,7 @@ export default class LinkedList {
 
   insertLast(value) {
     let node = new Node(value);
-    if(this.head == null) {
+    if(this.head === null) {
       this.head = node;
     } else {
       let temp = this.head;
@@ -20,7 +20,7 @@ export default class LinkedList {
 
   insertAtIndex(index, value) {
     let node = new Node(value);
-    if(index == 0) {
+    if(index === 0) {
       this.addFirst(value);
     } else {
       let curr = this.head;
@@ -29,7 +29,7 @@ export default class LinkedList {
         curr = curr.next;
         start++;
       }
-      if(curr == null) {
+      if(curr === null) {
         this.insertLast(value);
       } else {
         node.next = curr.next;
@@ -40,7 +40,7 @@ export default class LinkedList {
 
   addFirst(value) {
     let node = new Node(value);
-    if(this.head == null) {
+    if(this.head === null) {
       this.head = node;
     } else {
       node.next = this.head;
@@ -63,7 +63,7 @@ export default class LinkedList {
   }
 
   remove(index) {
-    if(index == 0 && this.head != null) {
+    if(index === 0 && this.head != null) {
       this.head = this.head.next;
     } else {
       let curr = this.head;
@@ -103,5 +103,153 @@ export default class LinkedList {
       count++;
     }
     return count;
+  }
+
+  reverse() {
+    let prev = null;
+    let curr = this.head;
+    let next = null;
+    while(curr != null) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr; 
+      curr = next;
+    }
+    return prev;
+  }
+}
+
+class SinglyLinkedList {
+  constructor() {
+    this.head = null;
+    this.tail = null;
+    this.length = 0;
+  }
+
+  push(value) {
+    let node = new Node(value);
+    if(this.head === null && this.tail === null) {
+      this.head = this.tail = node;
+    } else {
+      this.tail.next = node;
+      this.tail = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  pop() {
+    if(this.length === 0) {
+      return null;
+    } else {
+      let prev = null;
+      let curr = this.head;
+      while(curr.next != null) {
+        prev = curr;
+        curr = curr.next;
+      }
+      prev.next = null;
+      this.tail = prev;
+      this.length--;
+      if(this.length === 0) {
+        this.head = this.tail = null;
+      }
+      return curr;
+    }
+  }
+
+  shift() {
+    if(this.head === null) {
+      return null;
+    } else {
+      let oldHead = this.head;
+      this.head = oldHead.next;
+      this.length--;
+      return oldHead;
+    }
+  }
+
+  unshift(value) {
+    let node = new Node(value);
+    if(this.head === null && this.tail === null) {
+      this.head = this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head = node;
+    }
+    this.length++;
+    return this;
+  }
+
+  get(index) {
+    if(index< 0 || index >= this.length) {
+      return null;
+    }
+    let counter = 0;
+    let curr = this.head;
+    while(counter<index) {
+      curr = curr.next;
+      counter++;
+    }
+    return curr;
+  }
+
+  set(index, value) {
+    let foundNode = this.get(index);
+    if(foundNode) {
+      foundNode.data = value;
+      return true;
+    }
+    return false;
+  }
+
+  insert(index, value) {
+    if(index< 0 || index > this.length) {
+      return false;
+    }
+    if(index === this.length) {
+      return !!this.push(value);
+    }
+    if(index === 0) {
+      return !!this.unshift(value);
+    }
+    let node = new Node(value);
+    let curr = this.get(index-1);
+    node.next = curr.next;
+    curr.next = node;
+    this.length++;
+    return true;
+  }
+
+  remove(index) {
+    if(index< 0 || index >= this.length) {
+      return null;
+    }
+    if(index === 0) {
+      return this.shift();
+    }
+    if(index === this.length-1) {
+      return this.pop();
+    }
+    let curr = this.get(index-1);
+    let removed = curr.next;
+    curr.next = removed.next;
+    this.length--;
+    return removed;
+  }
+
+  reverse() {
+    let curr = this.head;
+    this.head = this.tail;
+    this.tail = curr;
+    let next = null;
+    let prev = null;
+    for(let i=0; i< this.length; i++) {
+      next = curr.next;
+      curr.next = prev;
+      prev = curr; 
+      curr = next;
+    }
+    return this;
   }
 }
